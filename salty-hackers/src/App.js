10fa4b8e14426542ac1181e413360ffc4891a2ec
brian leonard <bri2210433@yahoo.com>
2020-03-06 16:21:03 -0700

    auth working

--- salty-hackers/src/App.js
+++ salty-hackers/src/App.js
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 import NavContainer from "./components/NavBar/NavContainer"
 import SignUp from "./components/Registration/SignUp"
 import SignIn from "./components/Registration/SignIn"
-import { Route } from "react-router-dom";
-import AppContainer from './components/AppContainer'
+import { Route, Switch } from "react-router-dom";
+import AppContainer from './components/AppContainer';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
       <NavContainer />
       <Route exact path="/" component={SignIn}/>
       <Route exact path="/register" component={SignUp} />
-      <Route exact path="/app" component={AppContainer} />
+      <PrivateRoute exact path="/app" component={AppContainer} />
     </div>
   );
 }
---
+++ salty-hackers/src/components/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+    return axios.create({
+        headers: {
+            Authorization: sessionStorage.getItem('token')
+        }
+    })
+}
+
+export default axiosWithAuth;
