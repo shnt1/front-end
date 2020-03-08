c8e53975f44124f8046b9e4a1e9b98bcf9d9bda4
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-03-08 14:30:12 +0900

    removed password confirmation and checkbox

--- salty-hackers/src/components/Registration/SignUp.js
+++ salty-hackers/src/components/Registration/SignUp.js
@@ -32,7 +32,7 @@ function SignUp({ touched, errors }) {
               )}
            </label>
           </InputBox>
-          <InputBox>
+          {/* <InputBox>
             <label>
               <Field
                 type="password"
@@ -53,7 +53,7 @@ function SignUp({ touched, errors }) {
               )}
               By clicking here, you accept our terms of service.
             </label>
-          </CheckContainer>
+          </CheckContainer> */}
           <SubmitButton type="submit">Sign Up</SubmitButton>
         </Form>
       </MainBox>
@@ -75,11 +75,11 @@ export default withFormik({
       .required(),
     password: Yup.string()
       .min(3)
-      .required(),
-    confirm: Yup
-      .string()
-      .oneOf([Yup.ref("password"), null], "Passwords must match"),
-      checkbox: Yup.bool().oneOf([true], "You must accept the terms of service")
+      .required()
+    // confirm: Yup
+    //   .string()
+    //   .oneOf([Yup.ref("password"), null], "Passwords must match"),
+    //   checkbox: Yup.bool().oneOf([true], "You must accept the terms of service")
   }),
 
   handleSubmit(values, formikBag) {
@@ -268,7 +268,7 @@ const CheckContainer = styled.div`
 `;
 
 const SubmitButton = styled.button`
-    
+margin-top: 15px;
     padding: 5px 10px;
     font-size: 20px;
     text-align: center;
